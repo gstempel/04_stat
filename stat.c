aa0aec03aa9c8505d927ad75d3f9b7bef657c152
williamx <wxiang@stuy.edu>
2016-10-28 14:06:27 -0400

    kekerinos

--- stat.c
+++ stat.c
@@ -2,14 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <time.h>
 
 int main() {
 
   struct stat *foo = (struct stat*)malloc(sizeof(foo));
   stat("stat.c", foo);
   printf("FILE SIZE: %llu\n", foo->st_size);
-  printf("FILE PERMISSIONS: %d\n", foo->st_mode);
-  printf("LAST TIME ACCESS: %ld\n", foo->st_atime);
+  printf("FILE PERMISSIONS: %o\n", foo->st_mode);
 
+  struct tm *ktime = (struct tm*)malloc(sizeof(struct tm));
+  char buff[100];
+  *buff = asctime(gmtime(foo->st_atime));
+
+  //printf("LAST TIME ACCESSED: %d\n", &atime);
+  //char buff[20];
+  //strftime(buff, 20, "%Y-%m-%d %H:%M:%S", gmtime( foo->st_atime ));
+  //printf("LAST TIME ACCESSED: %s/n", buff);
+
+  //feint words
+
+  free(ktime);
+  free(foo);
   return 1;
 }
